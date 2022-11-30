5c4dbc78b704878d3a2a07bc5934b34bf13be63b
Newrmind <461496554QWERasdf>
2022-11-30 22:48:22 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+
+load_dotenv()
+
+bot = Bot(os.getenv('BOT_API'))
+dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print('Bot started successfully!')
+
+
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer(text='Добро пожаловать в главное меню')
+
+@dp.message_handler(commands=['links'])
+async def links_command(message: types.Message):
+    await message.answer(text='Choice option...!')
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher=dp,
+                           skip_updates=True,
+                           on_startup=on_startup)
+
